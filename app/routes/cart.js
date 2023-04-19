2aee5bbb402857755a9f9faaa4d01c51cd23a6d5
rmatej22 <rmatej545@gmail.com>
2023-04-19 16:13:01 +0200

    add controller and implement logic for controller

--- app/routes/cart.js
+++ app/routes/cart.js
@@ -1,3 +1,8 @@
 import Route from '@ember/routing/route';
 
-export default class CartRoute extends Route {}
+export default class CartRoute extends Route {
+  model() {
+    const items = [{ price: 20 }, { price: 25 }];
+    return items;
+  }
+}
